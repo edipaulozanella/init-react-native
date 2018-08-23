7c7faba01a31be4ad5c77ed54fa3212ef20cf1eb
Edipaulo Zanella <edipaulo@gmail.com>
2018-08-23 13:58:55 -0300

    ok

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-/** @format */
 
-// import {AppRegistry} from 'react-native';
-// import App from './App';
-// import {name as appName} from './app.json';
-// import App from './src/index.init';
-// import Index from './App';
-
-// AppRegistry.registerComponent("project", () => Index);
-
-// /** @format */
+import App from './src/index.init';
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent("project", () => App);
 
-
-//  
