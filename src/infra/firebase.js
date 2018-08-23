7004113075f2c79b24c9fe1a30396b01ecbf6b55
Edipaulo Zanella <edipaulo@gmail.com>
2018-08-23 14:06:21 -0300

    ok

--- src/infra/firebase.js
+++ src/infra/firebase.js
@@ -8,6 +8,7 @@ import {
   Alert
 } from "react-native";
 import { Cloud } from "../infra";
+import { showMessage } from "react-native-1app/lib/FlashMessage";
 
 import FCM, {
   FCMEvent,
@@ -18,45 +19,26 @@ import FCM, {
 var navigation = null;
 var globalStore = null;
 var subscrible = null;
+
 export function init(callback, store, nav) {
   navigation = nav;
   globalStore = store;
   FCM.requestPermissions(); // for iOS
   getToken(callback);
   configReceberNotificacoes((alert, data) => {
-    tratarNotificacao(alert, data);
+    // tratarNotificacao(alert, data);
   });
   FCM.setBadgeNumber(0);
   //   FCM.cancelAllLocalNotifications();
 }
-export function tratarNotificacao(alert, data) {
-  //   console.log("Dentro");
-  //   console.log(alert, data);
-  //   Alert.alert("Notificação", JSON.stringify(data));
-  if (data.opened_from_tray == 1 && navigation && data.type == "chat") {
-    navigation.navigate("PessoasChat", {});
-  }
-  // else {
-  //     console.log("naõ abre  pg");
-  //   }
-  if (globalStore && data.type == "chat") {
-    globalStore.dispatch({
-      type: "SOMAR_TOTAL_CHAT"
-    });
-  }
-  if (globalStore && data.type != "chat") {
-    globalStore.dispatch({
-      type: "SOMAR_NOTIFICACOES"
-    });
-  }
-}
+
 export function configReceberNotificacoes(callback) {
   AppState.addEventListener("change", state => {
     if (state == "active") FCM.setBadgeNumber(0);
   });
   if (subscrible) return;
   subscrible = FCM.on(FCMEvent.Notification, notif => {
-    console.log("Callbaci", notif);
+    if(__DEV__)console.log("Callbaci", notif);
     // Alert.alert("Notificação", JSON.stringify(notif))
     var alert = { title: "", body: "" };
     if (notif.fcm) {
@@ -64,7 +46,16 @@ export function configReceberNotificacoes(callback) {
     } else if (notif.aps && notif.aps.alert && notif.aps.alert.title) {
       alert = notif.aps.alert;
     }
-    if ((alert && alert.title) || notif.alert) {
+    if ((alert && (alert.title||alert.body)) || notif.alert) {
+      let act=notif.link?[{label:"ABRIR",onPress:()=>navigation.navigate("PgPdf", {activity: {}, oldState: {},url:notif.link}) }]:[];
+      showMessage({
+        message: alert.title,
+        description: alert.body,
+        type: "info",
+        backgroundColor:'#001E60',
+        icon:{ icon: "notifications_active", position: "left" },duration:8000,
+        actions:[...act,{label:"OK"}]
+      });
       Vibration.vibrate();
       if (callback) callback(alert, notif);
     }
